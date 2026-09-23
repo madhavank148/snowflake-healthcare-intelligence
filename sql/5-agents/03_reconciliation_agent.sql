12b3b3c0fc570a563da75b2db763ed156400166a
Madhavan k <54049740+madhavank148@users.noreply.github.com>
2026-09-23 15:08:12 +0530

    Update 03_reconciliation_agent.sql

--- sql/5-agents/03_reconciliation_agent.sql
+++ sql/5-agents/03_reconciliation_agent.sql
@@ -52,5 +52,7 @@ CREATE OR REPLACE AGENT SEMANTIC.RECONCILIATION_AGENT
   tool_resources:
     Reconciliation_Analyst:
       semantic_view: "HEALTHCARE_INTELLIGENCE_DB.SEMANTIC.SV_RECONCILIATION"
-      warehouse: "COMPUTE_WH"
+      execution_environment:
+        type: warehouse
+        warehouse: "COMPUTE_WH"
   $$;
